ce00b97220522e3fa000b94044808e6b9cbd0413
Fernando Zablah <zablah.developer@gmail.com>
2021-03-17 17:01:48 -0600

    agregue el metodo sumar

--- Calculadora.java
+++ Calculadora.java
@@ -2,4 +2,8 @@ class Calculadora{
     public static void main(String[] args) {
         System.out.println("11+1=12");
     }
+
+    public static int sumar(int a, int b){
+        return a+b;
+    }
 }
